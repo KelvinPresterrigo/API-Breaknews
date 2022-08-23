0c10cf6e029dcefdda87cc185a969950bbf69bf2
KelvinPresterrigo <kelvin42xd@gmail.com>
2022-08-23 14:38:50 -0300

    Rotas

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express')
 const app = express();
+//...+ Rotas
+//...+ Metodo HTTP - CRUDE(Create, Read, Update, Delete)
+// GET - pega uma info
+// POST - Cria uma info
+// PUT - altera toda info
+// PATH - Altera parte da info
+// DELETE - Apaga uma info 
+ 
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+    //...+ Name - Um identificador da Rota
+    //
+    //Function (Callback)- função por executar algo
+
+app.get('/soma', (req, res) => {
+  const soma = 1+1;
+   res.send({soma: soma})
+});
 app.listen(3000);
